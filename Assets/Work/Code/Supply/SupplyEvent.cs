17feb388ce8cc80bfa283c019254f30ac0772cfb
chuh007 <chhuh007@gmail.com>
2025-12-22 14:49:34 +0900

    feat : Supply

---
+++ Assets/Work/Code/Supply/SupplyEvent.cs
@@ -0,0 +1,16 @@
+﻿using Lib.Utiles;
+
+namespace Work.Code.Supply
+{
+    public class SupplyEvent : GameEvent
+    {
+        public SupplyType SupplyType { get; }
+        public int Amount { get; }
+
+        public SupplyEvent(SupplyType supplyType, int amount)
+        {
+            SupplyType = supplyType;
+            Amount = amount;
+        }
+    }
+}
---
+++ Assets/Work/Code/UI/SupplyUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Lib.Dependencies;
+using TMPro;
+using UnityEngine;
+using Work.Code.Supply;
+
+namespace Work.Code.UI
+{
+    [Serializable]
+    public struct SupplyText
+    {
+        public SupplyType type;
+        public TextMeshProUGUI text;
+    }
+    public class SupplyUI : MonoBehaviour
+    {
+        [SerializeField] private List<SupplyText> supplyTextSetting;
+        
+        [Inject] private UserSupplies _userSupplies;
+
+        private Dictionary<SupplyType, TextMeshProUGUI> _supplyTexts;
+
+        private void Awake()
+        {
+            _supplyTexts = new Dictionary<SupplyType, TextMeshProUGUI>();
+            foreach (var supplyText in supplyTextSetting)
+            {
+                _supplyTexts.Add(supplyText.type, supplyText.text);
+            }
+        }
+        
+        private void Start()
+        {
+            _userSupplies.OnSupplyChanged += HandleSupplyChange;
+        }
+
+        private void HandleSupplyChange(SupplyType supplyType, int value)
+        {
+            _supplyTexts[supplyType].SetText(value.ToString());
+        }
+    }
+}
