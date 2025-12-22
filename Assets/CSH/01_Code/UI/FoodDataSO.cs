6943dc8f13264d4bac09f65c7d7c72f0d62df794
dntlakfn <gun20010805@gmail.com>
2025-12-22 18:32:04 +0900

    fix : FoodDataSO

---
+++ Assets/CSH/01_Code/UI/FoodDataSO.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Work.Code.Supply;
+namespace CSH._01_Code.UI
+{
+    [CreateAssetMenu(fileName = "FoodData", menuName = "SO/FoodDataList")]
+    public class FoodData : ScriptableObject
+    {
+        public FoodType Type;
+        public string Name;
+        [TextArea]
+        public string Description;
+        public Supply[] Ingredients; // 재료 목록
+        public int Price;
+    }
+
+
+
+}
