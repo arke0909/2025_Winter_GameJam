a88ef8efe35851ddd750e97bfb88053d8a20875d
arke0909 <arke0909@gmail.com>
2025-12-23 16:26:52 +0900

    Fix : Remove 3 Cross

---
+++ Assets/Work/Code/Items/Remove3CrossNode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Work.Code.MatchSystem;
+
+namespace Work.Code.Items
+{
+    [CreateAssetMenu(fileName = "Remove3CrossNode", menuName = "SO/Item/Node/Remove 3CrossNode")]
+    public class Remove3CrossNode : EffectNodeSO
+    {
+        public override void Execute(MatchSystem.MatchSystem ms, NodeData data)
+        {
+            int x = data.Pos.x;
+            int y = data.Pos.y;
+            
+            for (int xn = -1; xn < 2; xn++)
+            {
+                int nx = x + xn;
+
+                ms.RemoveVertical(nx);
+            }
+            for (int yn = -1; yn < 2; yn++)
+            {
+                int ny = x + yn;
+
+                ms.RemoveHorizontal(ny);
+            }
+        }
+    }
+}
