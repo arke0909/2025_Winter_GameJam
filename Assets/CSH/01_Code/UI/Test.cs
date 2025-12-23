90a1b137ada4900e47aee0ef159f6721d753ac21
dntlakfn <gun20010805@gmail.com>
2025-12-23 15:30:11 +0900

    .

--- Assets/CSH/01_Code/UI/Test.cs
+++ Assets/CSH/01_Code/UI/Test.cs
@@ -1,14 +1,17 @@
+using Lib.ObjectPool.RunTime;
 using UnityEngine;
+using Work.Code.SoundSystem;
 namespace CSH._01_Code.UI
 {
     public class Test : MonoBehaviour
     {
         private Canvas canvas;
-
-
+        [SerializeField] private SoundSO sound;
+        [SerializeField] private PoolManagerMono poolManager;
+        [SerializeField] private PoolItemSO poolItem;
         private void Awake()
         {
-            
+            poolManager.Pop<SoundPlayer>(poolItem).PlaySound(sound);
         }
     }
 }
