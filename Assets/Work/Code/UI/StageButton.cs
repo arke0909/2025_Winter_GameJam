95091c2e752e46cb8a5278e216201d5ceb8439ed
chuh007 <chhuh007@gmail.com>
2025-12-23 17:38:55 +0900

    feat : StageIcon

--- Assets/Work/Code/UI/StageButton.cs
+++ Assets/Work/Code/UI/StageButton.cs
@@ -7,6 +7,8 @@ namespace Work.Code.UI
 {
     public class StageButton : MonoBehaviour
     {
+        [SerializeField] private Image icon;
+        [SerializeField] private Color disabledColor;
         private Button _button;
 
         private void Awake()
@@ -17,6 +19,7 @@ namespace Work.Code.UI
         public void ToggleButtonClick(bool isOn)
         {
             _button.interactable = isOn;
+            icon.color = isOn ? Color.white : disabledColor;
         }
         
         public void LoadScene(string sceneName)
